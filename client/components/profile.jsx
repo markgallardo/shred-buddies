5b8828609705693e7b276902563bcd5fdc897518
Jason Gonzales <jasongonzo25@gmail.com>
2020-11-12 19:31:53 -0800

    completed create profile-edit

--- client/components/profile.jsx
+++ client/components/profile.jsx
@@ -8,18 +8,9 @@ class Profile extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch(`/api/profile/${this.props.profile}`)
-      .then(res => res.json())
-      .then(profile =>
-        this.setState({
-          profile: profile
-        }))
-      .catch(err => console.error(err));
-  }
-
   render() {
-    if (!this.state.profile) {
+    const { name, email, skill, description, imgUrl } = this.props.profile;
+    if (!this.props.profile) {
       return null;
     }
 
@@ -27,13 +18,13 @@ class Profile extends React.Component {
       <div>
         <h2 className="ml-2 mt-2">Profile</h2>
         <div className="d-flex">
-          <img className="profile-pic m-auto" src={this.state.profile.imgUrl} />
+          <img className="profile-pic m-auto" src={imgUrl} />
         </div>
         <div className="card-body">
-          <h2 className="m-auto">{this.state.profile.name}</h2>
-          <p className="email">{this.state.profile.email}</p>
-          <p className="skill">{this.state.profile.skill}</p>
-          <p className="description">{this.state.profile.description}</p>
+          <h2 className="m-auto">{name}</h2>
+          <p className="email">{email}</p>
+          <p className="skill">{skill}</p>
+          <p className="description">{description}</p>
         </div>
       </div>
     );
