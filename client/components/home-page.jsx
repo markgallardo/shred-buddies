3f0dd167c256aa7b3ca8585f5992a8d07e2f04e6
Jason Gonzales <jasongonzo25@gmail.com>
2020-12-21 12:43:04 -0800

    complete

--- client/components/home-page.jsx
+++ client/components/home-page.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 export default function Homepage(props) {
   return (
+    <>
+      <div className="homepage-overlay"></div>
+      <div className="homepage-container">
 
-    <div className="homePage container" >
-
-      <div className="row d-flex align-items-center justify-content-center  ">
-        <h1 className="head-title d-flex text-center mt-5">SHRED  BUDDIES</h1>
-        <div className="mt-5 border">
-          {/* <img src="\images\logo.png" alt=""/> */}
-        </div>
-        <div className="row justify-content-center mt-5">
-          <button className="btn-detail ">LOG IN</button>
-          <button className="btn-detail  mt-3" onClick={() => props.setView('create', {})}>CREATE ACCOUNT</button>
+        <div className="row dropdown-itemd-flex align-items-center justify-content-center">
+          <h1 className="home-title d-flex text-center mt-5">SHRED  BUDDIES</h1>
+          <div className="mt-5 border">
+            {/* <img src="\images\logo.png" alt=""/> */}
+          </div>
+          <div className="row justify-content-center mt-5 home-btn">
+            <button className="btn-detail mb-2">LOG IN</button>
+            <button className="btn-detail" onClick={() => props.setView('create', {})}>CREATE ACCOUNT</button>
+          </div>
         </div>
       </div>
-    </div>
-
+    </>
   );
 }
