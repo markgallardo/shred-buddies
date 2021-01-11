9dd9946634fe86ad718183ed30e9839d3454d7e1
Jason Gonzales <jasongonzo25@gmail.com>
2021-01-10 21:39:34 -0800

    in progress

--- client/components/recommended-resort-detail.jsx
+++ client/components/recommended-resort-detail.jsx
@@ -35,13 +35,15 @@ export default class RecommendedResortDetail extends React.Component {
 
       <div className="justify-content-center resort-detail">
         <div className="d-flex flex-wrap">
-          <div className="mb-3 mx-2">
-            <h4><i className="fas fa-chevron-left pt-3"
+          <div className="mb-md-3 p-2 mx-2">
+            <h4><i className="fas fa-chevron-left py-md-3"
               onClick={() => this.props.setView('resortList', {})}></i> {this.state.resort.name}</h4>
+            <p className="mt-n1"> {this.state.resort.address}</p>
           </div>
-          <p className="mt-n4 mx-2 mb-3"> { this.state.resort.address}</p>
-          <p className="mt-n3 mx-2 py-2">{this.state.resort.description}</p>
-          <div className="ml-5 m-auto py-4">
+          <div className="mx-2 mt-n3">
+            <p>{this.state.resort.description}</p>
+          </div>
+          <div className="m-auto resort-img">
             <img src={this.state.resort.imgUrl} alt="" className="img-detail mt-n3"/>
           </div>
         </div>
